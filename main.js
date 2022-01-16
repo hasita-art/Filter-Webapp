55bb8d5b5068e57480bd40c70715e5c9e835324f
hasita-art <73806182+hasita-art@users.noreply.github.com>
2022-01-16 15:41:34 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-function preload(){
+noseX=0;
+noseY=0;
 
+function preload(){
+    mustache = loadImage("https://i.postimg.cc/3x3QzSGq/m.png");
 }
 
 function setup(){
@@ -14,19 +17,22 @@ function setup(){
 }
 
 function modelLoaded(){
-    console.log("poseNet is Initialized");
+    console.log("PoseNet is Initialized");
 }
 
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
-        console.log("nose x =" + results[0].pose.nose.x);
-        console.log("nose y =" + results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
     }
 }
 
 function draw(){
     image(video, 0, 0, 400, 400);
+    image(mustache, noseX, noseY+5, 60, 30);
 }
 
 function take_snapshot(){
